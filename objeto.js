7d23ab8d9663a66bcc7c30d890a7b47e9fa91cdb
Nickolas <nickolasandrew2204@gmail.com>
2025-02-19 08:46:27 -0300

    Add files via upload
    Conhecendo Objetos do Curso da Alura

---
+++ objeto.js
@@ -0,0 +1,12 @@
+const listaCPFs = ['12312312312', '123234435345'];
+
+const infosPessoa = ['nome', 'Jose Silva', 'idade', 32];
+
+console.log(infosPessoa[1]);
+
+const objPessoa = {
+    nome: 'Jose Silva',
+    idade: 32,
+    cpf: '12312312312',
+    turma: 'JavaScript'
+}
---
+++ objs-aninhados.js
@@ -0,0 +1,16 @@
+const estudante = {
+    nome: 'José Silva',
+    idade: 32,
+    cpf: '12312312312',
+    turma: 'JavaScript',
+    bolsista: true,
+    telefones: ['551199999998', '551199999993'],
+  }
+
+  estudante.endereço = {
+    rua: 'Rua Joseph Climber',
+    numero: '45',
+    complemento: 'Apto 43'
+  }
+
+console.log(estudante);
